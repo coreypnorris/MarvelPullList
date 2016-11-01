095abfdb00926c8b91a6f7ffaf24099c2ebd04d4
Corey Norris <coreypnorris@gmail.com>
2016-10-31 20:09:07 -0700

    initial commit

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name marvelPullListApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the marvelPullListApp
+ */
+angular.module('marvelPullListApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name marvelPullListApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the marvelPullListApp
+ */
+angular.module('marvelPullListApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
